7a87e336b653a0bf936f655e826519b43dfc358b
Sushmita Kumari <sushmitakumari@Sushmitas-MacBook-Air.local>
2021-10-31 23:35:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import profilePic from  './link_profile_pic.jpeg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="profilepic" src={profilePic} alt="pic"/>
+      <br></br>
+      <div className="profile_name">
+      <h1>Sushmita Kumari</h1>
+      
+      <h2>Assistant Professor</h2>
+      </div>
+      <a className="github_link" href="https://github.com/sushmita2109">Github</a>
+      <a className="linkedin_link" href="https://www.linkedin.com/in/sushmita-kumari-4393b2ab/">Linkedin</a>
+      <p className="details">Hi...Myself Sushmita ...This is a project using React js..
+        I have also done projects on android studio and python...
+      </p>
+
     </div>
   );
 }
